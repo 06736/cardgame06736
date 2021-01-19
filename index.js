dad3532cf11f5b2d2e8ed6474291143af62d8192
bmynard <06736@farnborough.ac.uk>
2021-01-19 16:22:16 +0000

    ggg

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+let deck = []; // the empty deck to be filled in the shuffle function
 function shuffle(){ //creates a shuffled array of the cards
-    let deck = [];
+
     let suit = ["red", "yellow", "black"]
     for (let i = 0; i < 3; i++){
         for(let j = 1;j<11;j++){
@@ -17,4 +18,34 @@ function shuffle(){ //creates a shuffled array of the cards
     console.log(deck)
 }
 
+let players = [
+    {
+        username: "John",
+        password: "Cena",
+        cards: [],
+
+    },
+    {
+        username: "Ben",
+        password: "Mynard",
+        cards: [],
+    }
+];
 shuffle()
+
+function login(){
+    event.preventDefault();
+    let username = $("#htmlUsername").val()
+    let password = $("#htmlPassword").val()
+    $(".input1").val("")
+    $("#p1").append(username)
+    for(let i = 0; i < 2;i++){
+        if(username === players[i][0]){
+            if(password === players[i][1]){
+                $("#p1").empty().append("Login Successful")
+            }
+        }
+    }
+
+}
+
